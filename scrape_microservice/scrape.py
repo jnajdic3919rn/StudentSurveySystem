d0de2c17c31035311dcf6c2d06303079f025a5ba
jnajdic3919rn <jnajdic3919rn@raf.rs>
2023-09-20 09:13:33 +0200

    Dodate provere za JWT

--- scrape_microservice/scrape.py
+++ scrape_microservice/scrape.py
@@ -4,11 +4,14 @@ from selenium.common.exceptions import WebDriverException
 import requests
 import time
 from bs4 import BeautifulSoup
+import jwt
 from flask import Flask, request
 from flask_cors import CORS
 import json
 import sys
 from rabbitmq_utils import setup_rabbitmq, publish_to_survey_queue, publish_to_comment_queue
+
+SECRET_KEY = 'najtajnovitijikljuczajwtkojijesvetikadvideo'
 
 app = Flask(__name__)
 CORS(app)
@@ -16,6 +19,34 @@ CORS(app)
 
 @app.route('/scrape', methods=['GET'])
 def scrape_data():
+    # Retrieve the "Authorization" header
+    authorization_header = request.headers.get('Authorization')
+
+    # Check if the header exists or is empty
+    if not authorization_header:
+        # Return a 401 Unauthorized status code with a message
+        return(401, 'Unauthorized')
+
+    try:
+        # Extract the token from the header (assuming it's in the format "Bearer <token>")
+        token = authorization_header.split()[1]
+
+        # Verify and decode the JWT using secret key
+        decoded_token = jwt.decode(token, SECRET_KEY, algorithms=['HS256'])
+
+        # You can access the claims from the decoded token as needed
+        roles = decoded_token.get('roles', [])
+
+        if 'ROLE_ADMIN' not in roles:
+            return(401, 'Unauthorized')
+    
+    except jwt.ExpiredSignatureError:
+        # Handle the case where the token has expired
+        return(401, 'Token has expired')
+    except jwt.DecodeError:
+        # Handle the case where the token is invalid or cannot be decoded
+        return(401, 'Invalid token')
+
     url = request.args.get('url')
     survey_name = request.args.get('survey')
     survey_type = request.args.get('surveyType')
@@ -40,18 +71,10 @@ def scrape_data():
     
     service = Service()
     
-    #service.executable_path = "./chromedriver/Linux/115/chromedriver"
-    print("serviceee")
-    
     options = webdriver.ChromeOptions()
-    #driver = webdriver.Chrome(service=service, options=options)
-    #options.add_argument(f"--webdriver-path={service.executable_path}")
-
-    #service_args = ['--log-path=/chromedriver.log']
 
     # Create the WebDriver instance with the specified options and service_args
     driver = webdriver.Chrome(options=options)
-    #driver = webdriver.Chrome(options=options, service_log_path='/chromedriver.log')
 
     try:
         driver.get(url)
@@ -69,13 +92,43 @@ def scrape_data():
         return response
 
     time.sleep(30)
-    print("uspeh")
+
+    current_url = driver.current_url
+
+    # Check if the current URL matches the original one
+    if current_url != url:
+        print("Unexpected navigation occurred.")
+        # The URL has changed, indicating unexpected navigation
+        driver.quit()
+        response_data = 'Došlo je do greške'
+        response = app.response_class(
+        response=json.dumps(response_data),
+        status=400,
+        mimetype='application/json'
+        )
+        return response
+        # Handle the situation as needed (e.g., show an error message or take appropriate action)
 
     # Extract cookies from the WebDriver
     cookies = driver.get_cookies()
 
     # Close the Selenium WebDriver
     driver.quit()
+
+    # Check if the user is logged in based on the cookies
+    if not cookies:
+        # No cookies found, indicating that the user did not log in within the timeout
+        print("User did not log in within the timeout")
+        response_data = 'Došlo je do greške'
+        response = app.response_class(
+        response=json.dumps(response_data),
+        status=400,
+        mimetype='application/json'
+        )
+        return response
+    else:
+        # Cookies found, indicating that the user logged in
+        print("User is logged in")
 
     # Convert cookies to requests-compatible format
     cookie_dict = {cookie['name']: cookie['value'] for cookie in cookies}
@@ -106,41 +159,12 @@ def scrape_data():
         section_name = link.get_text().strip()
         base_url = protected_url.rsplit('/', 1)[0]
         section_url = base_url + "/" + link.get('href')
-        print("SEKCIJAAAAA")
-        print(section_name)
+        
         if section_url:
             section_response = session.get(section_url)
             section_content = section_response.content.decode('utf-8')
 
-            grade_tag = "Ukupna prosečna ocena:"
-            grade_index = section_content.find(grade_tag)
-            end_index = section_content.find('<', grade_index)
-            if grade_index != -1:
-                grade = section_content[grade_index + len(grade_tag):end_index]
-                grade = grade.strip()
-                grade = float(grade)
-            else:
-                grade = 0.0
-            
-            votes_tag = "Ukupno glasalo:"
-            votes_index = section_content.find(votes_tag)
-            end_index = section_content.find('<', votes_index)
-            if votes_index != -1:
-                votes = section_content[votes_index + len(votes_tag):end_index]
-                votes = votes.strip()
-                votes = int(votes)
-            else:
-                votes = 0
-
-            print("Votes")
-            print(votes)
-            print("Grade")
-            print(grade)
-            subject_data = {
-                "subjectName": section_name,
-                "votes": int(votes),
-                "grade": float(grade)
-            }
+            subject_data = extract_survey_data_from_content(section_content=section_content, section_name=section_name)
 
             section_name_list.append(subject_data)
             
@@ -153,68 +177,40 @@ def scrape_data():
         "faculty": faculty
     }
 
-    send_to_data_microservice(survey_data)
+    send_to_data_microservice(survey_data, authorization_header)
 
     for link in section_links:
         section_name = link.get_text().strip()
         base_url = protected_url.rsplit('/', 1)[0]
         section_url = base_url + "/" + link.get('href')
-        print("SEKCIJAAAAA")
-        print(section_name)
+      
         if section_url:
             section_response = session.get(section_url)
             section_content = section_response.content.decode('utf-8')
 
-            #print(section_content)
-            # Define the target starting and ending tags
-            start_tag_1 = "<h3>Ostale kritike, sugestije i pohvale:</h3>"
-            start_tag_2 = "<h3>Ostale kritike, pohvale i sugestije:</h3>"
-            end_tag = "</body>"
-
-            found = False
-            # Find the starting index of the content
-            start_index = section_content.find(start_tag_1)
-            if start_index != -1:
-                found = True
-                
-            if found == False:
-                start_index = section_content.find(start_tag_2)
-                if start_index == -1:
-                    continue
-
-            # Find the ending index of the content
-            end_index = section_content.find(end_tag, start_index)
-            if end_index == -1:
-                content = section_content[start_index + len(start_tag_1):]
-            else:
-                # Extract and print the content
-                content = section_content[start_index + len(start_tag_1):end_index]
+            content_parts = extract_content_parts(section_content=section_content)
             
-            # Split the content by <br> tags
-            content_parts = content.split("<br>-----------------------------------------------------------------<br>")
-
             # Print each part
-            for part in content_parts:
-                message = part.strip()  # Get the message
-                if message:
+            if content_parts is not None:
+                for part in content_parts:
+                    message = part.strip()  # Get the message
+                    if message:
 
-                    # Convert JSON object to a string
-                    comment_data = {
-                        "surveyUrl": url,
-                        "subjectName": section_name,
-                        "comment": message,
-                        "faculty": faculty,
-                        "surveyType": survey_type
-                    }
+                        # Convert JSON object to a string
+                        comment_data = {
+                            "surveyUrl": url,
+                            "subjectName": section_name,
+                            "comment": message,
+                            "faculty": faculty,
+                            "surveyType": survey_type
+                        }
 
-                    publish_to_comment_queue(channel, comment_data)
+                        publish_to_comment_queue(channel, comment_data)
 
     connection.close()
     
     session.close()
-    response_data = {
-            "message": "Uspešno preuzeto! Rezultati ankete će uskoro biti dostupni."
-        }
+    response_data = "Uspešno preuzeto! Rezultati ankete će uskoro biti dostupni."
     response = app.response_class(
     response=json.dumps(response_data),
     status=200,
@@ -222,13 +218,72 @@ def scrape_data():
     )
     return response
 
-def send_to_data_microservice(data):
+def extract_survey_data_from_content(section_content, section_name):
+    grade_tag = "Ukupna prosečna ocena:"
+    grade_index = section_content.find(grade_tag)
+    end_index = section_content.find('<', grade_index)
+    if grade_index != -1:
+        grade = section_content[grade_index + len(grade_tag):end_index]
+        grade = grade.strip()
+        grade = float(grade)
+    else:
+        grade = 0.0
+
+    votes_tag = "Ukupno glasalo:"
+    votes_index = section_content.find(votes_tag)
+    end_index = section_content.find('<', votes_index)
+    if votes_index != -1:
+        votes = section_content[votes_index + len(votes_tag):end_index]
+        votes = votes.strip()
+        votes = int(votes)
+    else:
+        votes = 0
+
+    subject_data = {
+        "subjectName": section_name,
+        "votes": int(votes),
+        "grade": float(grade)
+    }
+
+    return subject_data
+
+
+def extract_content_parts(section_content):
+    # Define the target starting and ending tags
+    start_tag_1 = "<h3>Ostale kritike, sugestije i pohvale:</h3>"
+    start_tag_2 = "<h3>Ostale kritike, pohvale i sugestije:</h3>"
+    end_tag = "</body>"
+
+    found = False
+    # Find the starting index of the content
+    start_index = section_content.find(start_tag_1)
+    if start_index != -1:
+        found = True
+                
+    if not found:
+        start_index = section_content.find(start_tag_2)
+        if start_index == -1:
+            return None
+
+    # Find the ending index of the content
+    end_index = section_content.find(end_tag, start_index)
+    if end_index == -1:
+        content = section_content[start_index + len(start_tag_1):]
+    else:
+        # Extract and print the content
+        content = section_content[start_index + len(start_tag_1):end_index]
+    
+    # Split the content by <br> tags
+    content_parts = content.split("<br>-----------------------------------------------------------------<br>")
+
+    return content_parts
+
+
+def send_to_data_microservice(data, token):
     microservice_url = "http://localhost:8000/data/survey_info"
-    headers = {'Content-Type': 'application/json'}
-    #payload = json.dumps(data)
-
-    #print(payload)
-
+    headers = {'Content-Type': 'application/json',
+               'Authorization':token}
+    
     response = requests.post(microservice_url, json=data, headers=headers)
 
     if response.status_code == 200:
